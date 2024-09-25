4f2c14b64cea32ba3448a09c98e0bf71f233b1bb
Vlad Natrusnyi <vladnatrusnyi@gmail.com>
2024-09-25 16:46:48 +0300

    change config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getDatabase } from "firebase/database";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCgUM-QRLtP_NH6ryolUnT4RubnkqGOPys",
-    authDomain: "freelanseapp.firebaseapp.com",
-    projectId: "freelanseapp",
-    storageBucket: "freelanseapp.appspot.com",
-    messagingSenderId: "633057592490",
-    appId: "1:633057592490:web:f913263741339a19317cd3",
-    databaseURL: 'https://freelanseapp-default-rtdb.firebaseio.com'
-};
-
+const firebaseConfig = {};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
